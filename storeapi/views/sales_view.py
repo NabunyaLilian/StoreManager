5e7673d9bfb61c264b5f3ab6a9a0cf3fe1251816
Nabunya Lilian <liliannabunya2@gmail.com>
2018-10-24 16:16:00 +0300

    add validation file

--- storeapi/views/sales_view.py
+++ storeapi/views/sales_view.py
@@ -4,7 +4,6 @@
 from flask import request
 from flask_restful import Resource
 from storeapi.models.model import sales
-from storeapi.views.validation import validate
 
 class Sales(Resource):
     """
@@ -26,10 +25,10 @@ class Sales(Resource):
             price = data.get('price')
             date = data.get('date')
             store_attendant = data.get('store_attendant')
-            string_data2 = [name, store_attendant, date]
-            int_data2 = [quantity, price]
-            if validate(string_data2,int_data2) == True :
-                sale = {'sale_id':len(sales) +1, 'name': name, 'quantity':quantity, 'price':price, 'date':date, 'store_attendant':store_attendant}
-                sales.append(sale)
-                return sale, 201
-            return {"message":"Enter valid values please"}
+            string_data = [name, store_attendant, date]
+            int_data = [quantity, price]
+        if all(isinstance(x, str) for x in string_data) and all(isinstance(x, int) for x in int_data):
+            sale = {'sale_id':len(sales) +1, 'name': name, 'quantity':quantity, 'price':price, 'date':date, 'store_attendant':store_attendant}
+            sales.append(sale)
+            return sale, 201
+        return {"message":"Enter valid values please"}
