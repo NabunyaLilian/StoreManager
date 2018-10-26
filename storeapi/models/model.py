51f8cc788390edfd2aaf5ec42178d925f46a9dd1
Nabunya Lilian <liliannabunya2@gmail.com>
2018-10-26 08:32:29 +0300

    modify model.py

--- storeapi/models/model.py
+++ storeapi/models/model.py
@@ -1,3 +1,13 @@
 products = []
 sales = []
 
+users = [{'Id':1,'username':'Lia','password':'pwd', 'isadmin':'true'},
+
+         {'Id':2, 'username':'Jane','password':'web','isadmin':'false'}
+]
+
+def auth(usrname,password):
+    for usr in users:
+        if usr['username']==username and usr['password']==password and usr['isadmin']=="true":
+            
+
