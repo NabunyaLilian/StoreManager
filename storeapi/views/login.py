afbdb7f9b7de841b80b7472e13bae3a2dc2bc588
Nabunya Lilian <liliannabunya2@gmail.com>
2018-10-28 18:53:38 +0300

    save changes

---
+++ storeapi/views/login.py
@@ -0,0 +1,6 @@
+from flask_restful import Resource
+
+
+class LogIn(Resource):
+    def post(self):
+        
---
+++ storeapi/views/signup.py
@@ -0,0 +1,5 @@
+from flask_restful import Resource
+
+class SignUp(Resource):
+    def post(self):
+        
